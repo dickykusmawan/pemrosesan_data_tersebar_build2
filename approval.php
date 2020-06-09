65d645e1ede0ca2ff7af7de59d6d37dcc2a2aaa8
DICKY KUSMAWAN <kusmawand@id.corp.seagroup.com>
2020-06-09 11:59:34 +0700

    New

--- approval.php
+++ approval.php
@@ -102,47 +102,6 @@
                     }
                   }
                   ?>
-
-
-                      <!-- <tr>
-                        <td>20170801374</td>
-                        <td>Dicky Kusmawan</td>
-                        <td>01-06-2020</td>
-                        <td><span class="tag tag-success">Pending</span></td>
-                        <td>Ganti Battrei Server</td>
-                        <td>
-                          <button type="button" class="btn btn-block btn-primary btn-sm">Accept</button>
-                        </td>
-                        <td>
-                          <button type="button" class="btn btn-block btn-danger btn-sm">Reject</button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>20170801374</td>
-                        <td>Dicky Kusmawan</td>
-                        <td>02-06-2020</td>
-                        <td><span class="tag tag-success">Pending</span></td>
-                        <td>Ganti Kabel Listrik Server</td>
-                        <td>
-                          <button type="button" class="btn btn-block btn-primary btn-sm" onclick="location.href = 'login.php';">Accept</button>
-                        </td>
-                        <td>
-                          <button type="button" class="btn btn-block btn-danger btn-sm">Reject</button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>20170801374</td>
-                        <td>Dicky Kusmawan</td>
-                        <td>03-06-2020</td>
-                        <td><span class="tag tag-success">Pending</span></td>
-                        <td>Ganti Kabel Lan Server</td>
-                        <td>
-                          <button type="button" class="btn btn-block btn-primary btn-sm">Accept</button>
-                        </td>
-                        <td>
-                          <button type="button" class="btn btn-block btn-danger btn-sm">Reject</button>
-                        </td>
-                      </tr> -->
                     </tbody>
                   </table>
                 </div>
@@ -159,79 +118,5 @@
   <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
-
-<script>
-    $(function () {
-      //Initialize Select2 Elements
-      $('.select2').select2()
-  
-      //Initialize Select2 Elements
-      $('.select2bs4').select2({
-        theme: 'bootstrap4'
-      })
-  
-      //Datemask dd/mm/yyyy
-      $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
-      //Datemask2 mm/dd/yyyy
-      $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
-      //Money Euro
-      $('[data-mask]').inputmask()
-  
-      //Date range picker
-      $('#reservationdate').datetimepicker({
-          format: 'L'
-      });
-      //Date range picker
-      $('#reservation').daterangepicker()
-      //Date range picker with time picker
-      $('#reservationtime').daterangepicker({
-        timePicker: true,
-        timePickerIncrement: 30,
-        locale: {
-          format: 'MM/DD/YYYY hh:mm A'
-        }
-      })
-      //Date range as a button
-      $('#daterange-btn').daterangepicker(
-        {
-          ranges   : {
-            'Today'       : [moment(), moment()],
-            'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-            'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-          },
-          startDate: moment().subtract(29, 'days'),
-          endDate  : moment()
-        },
-        function (start, end) {
-          $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-        }
-      )
-  
-      //Timepicker
-      $('#timepicker').datetimepicker({
-        format: 'LT'
-      })
-      
-      //Bootstrap Duallistbox
-      $('.duallistbox').bootstrapDualListbox()
-  
-      //Colorpicker
-      $('.my-colorpicker1').colorpicker()
-      //color picker with addon
-      $('.my-colorpicker2').colorpicker()
-  
-      $('.my-colorpicker2').on('colorpickerChange', function(event) {
-        $('.my-colorpicker2 .fa-square').css('color', event.color.toString());
-      });
-  
-      $("input[data-bootstrap-switch]").each(function(){
-        $(this).bootstrapSwitch('state', $(this).prop('checked'));
-      });
-  
-    })
-  </script>
 </body>
 </html>
